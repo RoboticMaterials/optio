f1b4f55a18d40b07b49af546b571824783a61750
Justin Baca <justin@roboticmaterials.com>
2020-12-14 10:05:42 -0700

    commit before switch to master

--- src/App.js
+++ src/App.js
@@ -67,7 +67,7 @@ const App = (props) => {
             return (
                 <Route
                     path={["/locations/:stationID?/:widgetPage?", '/']}>
-                    <div mobileMode={mobileMode} />
+                    <MapView mobileMode={mobileMode} />
                 </Route>
             )
         }
@@ -177,7 +177,7 @@ const App = (props) => {
                                                 :
                                                 <Route
                                                     path={["/locations/:stationID?/:widgetPage?", '/']}
-                                                    component={null}
+                                                    component={MapView}
                                                 />)
 
                                                 :
