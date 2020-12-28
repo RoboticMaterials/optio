d9dd3d57c0370a0065dc681e19fea54ea8c3181f
Justin Baca <justin@roboticmaterials.com>
2020-12-28 14:28:11 -0700

    fix issue with lots selector in hil_modals constantly reopening automatically

--- src/components/hil_modals/hil_modals.js
+++ src/components/hil_modals/hil_modals.js
@@ -70,6 +70,7 @@ const HILModals = (props) => {
     const [selectedDashboard, setSelectedDashboard] = useState(null)
     const [cardsLoaded, setCardsLoaded] = useState([false])
     const [showLotSelector, setShowLotSelector] = useState(false)
+    const [didDisplayLots, setDidDisplayLots] = useState(false)
     const [hilLoadUnload, setHilLoadUnload] = useState('')
 
     const {
@@ -112,8 +113,12 @@ const HILModals = (props) => {
 
     // load card data on load for selecting lot
     useEffect(() => {
-        if (cardsLoaded && availableLots.length > 0) setShowLotSelector(true)
-    }, [cardsLoaded, availableLots])
+        if (cardsLoaded && !didDisplayLots && availableLots.length > 0) {
+            setShowLotSelector(true)
+            setDidDisplayLots(true)
+        }
+
+    }, [cardsLoaded, availableLots, didDisplayLots])
 
     /*
     * Get dropdownsearch options for cards
