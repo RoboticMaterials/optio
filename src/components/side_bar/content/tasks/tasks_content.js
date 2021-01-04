69284969fa04b41e9d8cc847231a00a49e84a166
Kalervo <kalervo@Robs-Computer>
2021-01-04 11:09:25 -0700

    Handoff fix for device enabled map

--- src/components/side_bar/content/tasks/tasks_content.js
+++ src/components/side_bar/content/tasks/tasks_content.js
@@ -165,7 +165,7 @@ export default function TaskContent(props) {
                         type: 'push',
                         quantity: 1,
                         device_type: !!MiRMapEnabled ? 'MiR_100' : 'human',
-                        handoff: true,
+                        handoff: !!MiRMapEnabled ? false : true,
                         track_quantity: true,
                         map_id: currentMap._id,
                         new: true,
