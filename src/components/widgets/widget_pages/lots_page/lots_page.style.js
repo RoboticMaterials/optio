f07d03676750f049edb5384e128ec8a0dc27279a
Rob Lewis <rob@roboticmaterials.com>
2021-01-07 16:50:35 -0700

    lots page styling scrolling and add end date

--- src/components/widgets/widget_pages/lots_page/lots_page.style.js
+++ src/components/widgets/widget_pages/lots_page/lots_page.style.js
@@ -13,8 +13,17 @@ export const LotsContainer = styled.div`
     width: 100%;
     height: 100%;
     justify-content: center;
+    padding-bottom:1rem;
 
     background: ${props => props.theme.bg.quaternary};
+
+    overflow: auto;
+
+    // optionally style scroll bar
+    ::-webkit-scrollbar {
+    }
+    ::-webkit-scrollbar-thumb {
+    }
 
 `
 
@@ -53,7 +62,6 @@ export const ListSubtitle = styled.div`
 
 export const ListContent = styled.div`
     text-align: flex-start;
-    width: 100%;
     height:100%;
 
     font-family: ${props => props.theme.font.primary};
@@ -108,11 +116,19 @@ export const RowContainer = styled.div`
 
 export const ColumnContainer1 = styled.div`
     display: flex;
-    flex: 1;
+    flex: 4;
     flex-direction: column;
     justify-content: flex-start;
     height: 100%;
     padding-left: 1rem;
+
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+
+    @media only screen and (max-width: ${props => props.theme.widthBreakpoint.tablet}) {
+      flex:1;
+    }
 `
 
 export const ColumnContainer2 = styled.div`
@@ -134,20 +150,4 @@ export const ListItemIcon = styled.i`
         cursor: pointer;
         color:yellow;
     }
-`
-
-export const ListContainer = styled.div`
-	width: 100%;
-	flex: 1;
-
-    padding: .1rem;
-    padding-top: 1.5rem;
-
-	overflow: auto;
-
-	// optionally style scroll bar
-	::-webkit-scrollbar {
-	}
-	::-webkit-scrollbar-thumb {
-	}
 `
