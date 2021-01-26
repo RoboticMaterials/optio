aa8a0a4c05914ced8c32ad09940b1adfeaaa79bf
Rob Lewis <rob@roboticmaterials.com>
2021-01-26 11:57:26 -0700

    change map data back to 1000

--- src/containers/api_container/api_container.js
+++ src/containers/api_container/api_container.js
@@ -119,7 +119,7 @@ const ApiContainer = (props) => {
         // this interval is always on
         // loads essential info used on every page such as status and taskQueue
         setCriticalDataInterval(setInterval(() => loadCriticalData(), 500));
-        setMapDataInterval(setInterval(() => loadMapData(), 500));
+        setMapDataInterval(setInterval(() => loadMapData(), 1000));
 
 
         return () => {
