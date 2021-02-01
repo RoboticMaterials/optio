05dbd4c79e15ffcde28bb7576cd9941b0e5254df
Kalervo <KalervoHyyppa@gmail.com>
2021-02-01 14:53:49 -0700

    Reset Constants

--- src/constants/local_settings_constants.js
+++ src/constants/local_settings_constants.js
@@ -1,8 +1,8 @@
 import {isBrowser} from "react-device-detect";
 
 export const defaultLocalSettings = {
-	non_local_api_ip: '10.0.0.186',
-	non_local_api: true,
+	non_local_api_ip: '',
+	non_local_api: false,
 	mapViewEnabled: isBrowser,
 	toggleDevOptions:false,
 	currentMapId: null,
