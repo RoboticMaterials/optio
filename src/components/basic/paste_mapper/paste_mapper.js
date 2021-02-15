1e431e2715d0ded253a25df7969fb899bb5d8b1e
Justin Baca <justinbaca@Justins-MacBook-Pro-2.local>
2021-02-15 07:33:30 -0700

    commit before making remote branch

--- src/components/basic/paste_mapper/paste_mapper.js
+++ src/components/basic/paste_mapper/paste_mapper.js
@@ -1,6 +1,11 @@
-import React, {useEffect, useRef, useState} from 'react'
+import React, {useContext, useEffect, useRef, useState} from 'react'
 import PropTypes from "prop-types";
 import * as styled from './paste_mapper.style'
+import Button from "../button/button";
+import ButtonGroup from "../button_group/button_group";
+import {isArray} from "../../../methods/utils/array_utils";
+import Textbox from "../textbox/textbox";
+import {ThemeContext} from "styled-components";
 
 const PasteMapper = (props) => {
 
@@ -8,23 +13,88 @@ const PasteMapper = (props) => {
 		table
 	} = props
 
-	console.log("PasteMapper table",table)
 	const [fieldLabels, setFieldLabels] = useState([])
+	const [fieldDirection, setFieldDirection] = useState(0)
+
+	// theme
+	const themeContext = useContext(ThemeContext);
+
 
 	const renderTable = () => {
 		return (
 			<styled.Table>
+				{
+				<styled.Column>
+					{isArray(table[0]) && table[0].map((junk, currIndex) => {
+
+						if(fieldDirection === 1) {
+							return(
+								<styled.ItemContainer>
+									<Textbox
+										placeholder={"Field name..."}
+										style={{
+											background: themeContext.bg.tertiary,
+											maxHeight: "2rem"
+										}}
+										textboxContainerStyle={{
+											maxHeight: "2rem"
+										}}
+
+									/>
+								</styled.ItemContainer>
+							)
+						}
+						else {
+							if(currIndex === 0) {
+								return(
+									<styled.ItemContainer>
+									<div>Use Field Names</div>
+									</styled.ItemContainer>
+								)
+							}
+							else {
+								return(
+									<styled.ItemContainer>
+										->
+									</styled.ItemContainer>
+								)
+							}
+
+
+						}
+
+					})}
+				</styled.Column>
+
+				}
 				{table.map((currRow, currRowIndex) => {
 					return(
-						<styled.Row>
+						<styled.Column>
 							{currRow.map((currItem, currItemIndex) => {
 								return(
+									<>
+										{(currItemIndex === 0 && fieldDirection === 0) &&
+										<styled.ItemContainer>
+											<Textbox
+												placeholder={"Field name..."}
+												style={{
+													background: themeContext.bg.tertiary,
+													maxHeight: "2rem"
+												}}
+												textboxContainerStyle={{
+													maxHeight: "2rem"
+												}}
+
+											/>
+										</styled.ItemContainer>
+										}
 									<styled.ItemContainer>
 										{currItem}
 									</styled.ItemContainer>
+									</>
 								)
 							})}
-						</styled.Row>
+						</styled.Column>
 					)
 				})}
 			</styled.Table>
@@ -33,6 +103,21 @@ const PasteMapper = (props) => {
 
 	return (
 		<styled.Container>
+			<styled.Header>
+				<ButtonGroup
+					buttonViewCss={styled.buttonViewCss}
+					buttons={["Row", "Column"]}
+					selectedIndex={fieldDirection}
+					onPress={(index)=>{
+						setFieldDirection(index)
+					}}
+					containerCss={styled.buttonGroupContainerCss}
+					buttonViewSelectedCss={styled.buttonViewSelectedCss}
+					buttonCss={styled.buttonCss}
+				/>
+			</styled.Header>
+
+
 			{renderTable()}
 		</styled.Container>
 	)
--- src/components/basic/paste_mapper/paste_mapper.style.js
+++ src/components/basic/paste_mapper/paste_mapper.style.js
@@ -2,35 +2,114 @@ import styled, {css} from "styled-components"
 
 export const Container = styled.div`
 	display: flex;
-  background: red;
+  //background: red;
   min-height: 20%;
   min-width: 80%;
+  flex-direction: column;
   position: absolute;
   top:50%;
   left: 50%;
   transform: translate(-50%, -50%);
   z-index: 5000;
+  padding: 1rem;
+  overflow: hidden;
+  max-height: 80%;
+  
+  background: ${props => props.theme.bg.quinary};
 `
+
+export const Header = styled.div`
+
+`
+
+
+
 
 export const Table = styled.div`
 	display: flex;
   flex: 1;
   overflow: auto;
   //flex-direction: column;
-  background: yellow;
+  //background: yellow;
   
 `
 
-export const Row = styled.div`
+export const Column = styled.div`
   flex-direction: column;
   align-self:stretch;
   flex: 1;
 	display: flex;
-  	background: green;
+  border: 1px solid ${props => props.theme.bg.secondary};
+  	//background: green;
+`
+
+export const Row = styled.div`
+  align-self:stretch;
+  flex: 1;
+	display: flex;
+  border: 1px solid ${props => props.theme.bg.secondary};
+  	//background: green;
 `
 
 export const ItemContainer = styled.div`
-	background: blue;
+	//background: blue;
+  overflow: hidden;
+  border-top: 1px solid ${props => props.theme.bg.secondary};
+  border-bottom: 1px solid ${props => props.theme.bg.secondary};
+  border-radius: .5rem;
+  text-align: center;
+  
   height: 2rem;
+  max-height: 2rem;
+  background: ${props => props.theme.bg.quaternary};
+  
   //width: 10rem;
+`
+
+
+
+export const buttonViewCss = css`
+	//border-right: ${props => !props.isLast && `solid ${props.theme.bg.quaternary} thin`}; // dont show border on last item
+	color: ${props => props.theme.bg.quinary};
+	padding: 0;
+	margin: 0;
+  margin: 0 .25rem;
+	padding-left: .5rem;
+	padding-right: .5rem;
+  background: ${props => props.theme.bg.senary};
+  
+  &:hover {
+	cursor: pointer;
+  }
+`
+
+
+export const buttonViewSelectedCss = css`
+	//background: transparent;
+  background: ${props => props.theme.bg.secondary};
+	color: white;
+`
+export const buttonCss = css`
+	margin: 0;
+	padding: 0;
+	
+	&:focus{
+	}
+	
+	&:active{
+	}
+	
+	&:hover{
+		cursor: default;
+	}
+	
+`
+
+export const buttonGroupContainerCss = css`
+	display: flex;
+	flex-direction: row;
+	align-self: center;
+	padding: 0;
+	margin: 0;
+	
 `
