9c67d4fcf21333f7172655f62329d57d1aff359c
danoro96 <danoro96@gmail.com>
2021-02-18 13:33:36 -0700

    change local settings

--- src/constants/local_settings_constants.js
+++ src/constants/local_settings_constants.js
@@ -1,9 +1,9 @@
 import {isBrowser} from "react-device-detect";
 
 export const defaultLocalSettings = {
-	non_local_api_ip: '',
-	non_local_api: false,
-	mapViewEnabled: true,
+	non_local_api_ip: '18.220.200.169',
+	non_local_api: true,
+	mapViewEnabled: isBrowser,
 	toggleDevOptions:false,
 	currentMapId: null,
 	authenticated: null
