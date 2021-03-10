ba02658a079d87f1e8c03d86c28acbf73fcdaa5f
Justin Baca <justinbaca@Justins-MacBook-Pro-2.local>
2021-03-10 14:51:33 -0700

    test

--- src/App.js
+++ src/App.js
@@ -159,6 +159,18 @@ const App = () => {
                                       }
                                   </styled.HeaderContainer>
 
+                                  <div
+                                      style={{
+                                          position: "absolute",
+                                          color: "red",
+                                          fontSize: "20rem",
+                                          left: "50%",
+                                          top: "50%",
+                                          zIndex: 50000
+                                      }}
+
+                                  >TEST</div>
+
 
 
                                   <styled.BodyContainer>
