20a8018282ba070f17f6f94319f0fc3fe61eab9f
Daniel Castillo <daniel@roboticmaterials.com>
2021-03-18 10:08:30 -0600

    working version for organizations in tasks

--- src/api/tasks_api.js
+++ src/api/tasks_api.js
@@ -1,24 +1,40 @@
+/**
+ * All of the API calls for Tasks
+ * 
+ * Created: ?
+ * Created by: ?
+ * 
+ * Edited: March 18 20201
+ * Edited by: Daniel Castillo
+ * 
+ */
+
 // logging for error in API
 import errorLog from './errorLogging'
 
 // import the API category from Amplify library
 import { API } from 'aws-amplify'
 
+import getUserOrgId from './user_api'
+
 // import the GraphQL queries, mutations and subscriptions
-import { listTasks } from '../graphql/queries';
+import { tasksByOrgId } from '../graphql/queries';
 import { createTask, updateTask } from '../graphql/mutations';
 import { deleteTask as deleteTaskByID } from '../graphql/mutations';
 
 export async function getTasks() {
   try {
 
+    const userOrgId = await getUserOrgId()
+
     const res = await API.graphql({
-      query: listTasks
+      query: tasksByOrgId,
+      variables: { organizationId: userOrgId }
     })
 
     let GQLdata = []
 
-    res.data.listTasks.items.forEach(task => {
+    res.data.TasksByOrgId.items.forEach(task => {
       GQLdata.push( {
         ...task,
         device_types: JSON.parse(task.device_types),
@@ -37,9 +53,13 @@ export async function getTasks() {
 
 export async function getTask(id) {
   try {
+
+    const userOrgId = await getUserOrgId()
+
     const res = await API.graphql({
-      query: listTasks,
+      query: tasksByOrgId,
       variables:{
+        organizationId: userOrgId,
         filter: {_id: {eq: id}}
       }
     })
@@ -53,8 +73,12 @@ export async function getTask(id) {
 
 export async function postTask(task) {
   try {
+
+    const userOrgId = await getUserOrgId()
+
     const input = {
       ...task,
+      organizationId: userOrgId,
       device_types: JSON.stringify(task.device_types),
       processes: JSON.stringify(task.processes),
       load: JSON.stringify(task.load),
@@ -67,7 +91,9 @@ export async function postTask(task) {
       variables: { input: input }
     })
 
-    return dataJson;
+    console.log(dataJson)
+
+    return dataJson.data.createTask;
   } catch (error) {
     // Error 😨
     errorLog(error)
@@ -77,16 +103,19 @@ export async function postTask(task) {
 export async function deleteTask(id) {
   try {
 
+    const userOrgId = await getUserOrgId()
+
     const res = await API.graphql({
-      query: listTasks,
+      query: tasksByOrgId,
       variables:{
+        organizationId: userOrgId,
         filter: {_id: {eq: id}}
       }
     })
 
     const dataJson = await API.graphql({
       query: deleteTaskByID,
-      variables: { input: {id: res.data.listTasks.items[0].id} }
+      variables: { input: {id: res.data.TasksByOrgId.items[0].id} }
     })
 
     return dataJson;
