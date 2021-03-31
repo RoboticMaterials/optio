b2a24c7efdd102fc4d430cc0b59aba81773c916d
Kalervo <kalervo@Robs-Computer>
2021-03-31 15:45:03 -0600

    Added some default values that where missing

--- src/methods/utils/map_utils.js
+++ src/methods/utils/map_utils.js
@@ -7,7 +7,6 @@ export const convertD3ToReal = ([x, y], d3) => {
 }
 
 export const convertRealToD3 = ([pos_x, pos_y], d3) => {
-
     const x = d3.translate[0] + d3.scale * pos_x / (d3.mapResolution)
     const y = d3.translate[1] + d3.scale * (d3.naturalDims.height - (pos_y / d3.mapResolution))
 
