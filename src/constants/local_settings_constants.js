6a1b71e95aba0244ffe44129d47a0d28940b2061
Kalervo <kalervo@Robs-Computer>
2021-04-07 15:11:34 -0600

    Default to lsit view when loading into RMstudio via mobiel device

--- src/constants/local_settings_constants.js
+++ src/constants/local_settings_constants.js
@@ -1,11 +1,13 @@
+import { isMobile } from "react-device-detect";
+
 
 export const defaultLocalSettings = {
-	non_local_api_ip: '',
-	non_local_api: false,
-	mapViewEnabled: true,
-	toggleDevOptions:false,
-	currentMapId: null,
-	authenticated: null,
-	refreshToken: null,
-	accessToken: null,
+    non_local_api_ip: '',
+    non_local_api: false,
+    mapViewEnabled: isMobile ? false : true,
+    toggleDevOptions: false,
+    currentMapId: null,
+    authenticated: null,
+    refreshToken: null,
+    accessToken: null,
 }
