cc2ca3499976b9ae9bba40e4fb314173710b2758
Kalervo <KalervoHyyppa@gmail.com>
2021-05-11 11:50:45 -0600

    Adding to header

--- src/components/widgets/widget_pages/statistics_page/statistics_page.js
+++ src/components/widgets/widget_pages/statistics_page/statistics_page.js
@@ -7,6 +7,9 @@ import * as styled from './statistics_page.style'
 // Import Components
 import StatisticsOverview from './statistics_overview/statistics_overview'
 import StatisticsCharts from './statistics_charts/statistics_charts'
+import TimePicker from "rc-time-picker";
+import Button from '../../../basic/button/button'
+// import TimePickerField from '../../../basic/form/time_picker_field/time_picker_field'
 
 // Import Actions
 import moduleName from '../../../../redux/actions/'
@@ -27,11 +30,36 @@ const StatisticsPage = () => {
         }
     }, [])
 
+    const renderCycleTime = () => {
+        return (
+            <styled.HeaderSection>
+                <styled.HeaderSectionTitle>
+                    Cycle Time
+                </styled.HeaderSectionTitle>
+                <TimePicker
+                    showHours={true}
+                    showMinutes={true}
+                    onChange={(val) => {
+                        console.log('QQQQ change', val)
+                    }}
+                />
+                <Button
+                    label={'Save'}
+                    onClick ={() => {
+
+                    }}
+                    schema={'statistics'}
+                />
+
+            </styled.HeaderSection>
+        )
+    }
+
     return (
         <styled.StatisticsContainer>
             <styled.Header>
                 <styled.StationName>{stations[stationID].name}</styled.StationName>
-
+                {renderCycleTime()}
                 {/* <styled.StatisticsSectionsButtonContainer>
                     <styled.StatisticsSectionsButton
                         style={{ borderRadius: '.5rem 0rem 0rem .5rem' }}
