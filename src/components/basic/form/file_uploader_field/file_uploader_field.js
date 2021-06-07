7fc7de03fc51de84f7f525cd56e4dbc3d8db6e79
Justin Baca <justin@roboticmaterials.com>
2021-06-07 16:39:01 -0600

    update field uploader field

--- src/components/basic/form/file_uploader_field/file_uploader_field.js
+++ src/components/basic/form/file_uploader_field/file_uploader_field.js
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import PropTypes from 'prop-types';
 import FileUploader from "../../file_uploader/file_uploader";
+import {useField, useFormikContext} from "formik";
+import {ThemeContext} from "styled-components";
+import {getMessageFromError} from "../../../../methods/utils/form_utils";
 
 const FileUploaderField = (props) => {
+
+    const { setFieldValue, setFieldTouched, validateOnChange, validateOnBlur, validateField, status, validateForm, ...context } =  useFormikContext() || {}
+    const [field, meta] = useField(props);
+    const { touched, error } = meta
+    const {
+        name: fieldName
+    } = field
+
+    const {
+        warnings
+    } = status || {}
+
+    const {
+        [fieldName]: warning
+    } = warnings || {}
+
+    const themeContext = useContext(ThemeContext)
+
+    const hasError = touched && error
+    const hasWarning = touched && warning
+
+    const errorMessage = getMessageFromError(error)
+    const warningMessage = getMessageFromError(warning)
+
     return (
         <FileUploader
 
