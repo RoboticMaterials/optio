4d154166266558eefc81dda000f056cf2a0ffaca
KalervoHyyppa <kalervohyyppa@gmail.com>
2021-06-29 09:35:09 -0600

    Format and some comments

--- src/api/apollo_client.js
+++ src/api/apollo_client.js
@@ -1,154 +1,154 @@
-import {ApolloClient, InMemoryCache, createHttpLink, split, from, ApolloLink} from '@apollo/client';
+import { ApolloClient, InMemoryCache, createHttpLink, split, from, ApolloLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 import * as AmazonCognitoIdentity from 'amazon-cognito-identity-js';
 import { WebSocketLink } from '@apollo/client/link/ws';
-import {getMainDefinition} from "@apollo/client/utilities";
-import {onError} from "@apollo/client/link/error";
+import { getMainDefinition } from "@apollo/client/utilities";
+import { onError } from "@apollo/client/link/error";
 
 const cache = new InMemoryCache();
 
 const customFetch = (uri, options) => {
-	const { operationName } = JSON.parse(options.body);
-	console.log('customFetch',{uri, options})
-	return fetch(uri, options);
+    const { operationName } = JSON.parse(options.body);
+    console.log('customFetch', { uri, options })
+    return fetch(uri, options);
 };
 
 const httpLink = createHttpLink({
-	uri: process.env.REACT_APP_GQL_API_ENDPOINT,
-	fetch: customFetch
+    uri: process.env.REACT_APP_GQL_API_ENDPOINT,
+    fetch: customFetch
 });
 
 const errorLink = onError(({
-							   graphQLErrors,
-							   networkError,
-							   serverError
+    graphQLErrors,
+    networkError,
+    serverError
 }) => {
-	if (graphQLErrors)
-		graphQLErrors.forEach(({ message, locations, path }) =>
-			console.error(
-				`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
-			),
-		);
+    if (graphQLErrors)
+        graphQLErrors.forEach(({ message, locations, path }) =>
+            console.error(
+                `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+            ),
+        );
 
-	if (networkError) console.error(`[Network error]: ${networkError}`);
+    if (networkError) console.error(`[Network error]: ${networkError}`);
 });
 
 const getAuthToken = () => {
-	let token
+    let token
 
-	const poolData = {
-		UserPoolId: process.env.REACT_APP_POOL_ID,
-		ClientId: process.env.REACT_APP_POOL_CLIENT,
-	}
+    const poolData = {
+        UserPoolId: process.env.REACT_APP_POOL_ID,
+        ClientId: process.env.REACT_APP_POOL_CLIENT,
+    }
 
-	var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
-	var cognitoUser = userPool.getCurrentUser();
+    var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+    var cognitoUser = userPool.getCurrentUser();
 
-	if (cognitoUser != null) {
-		cognitoUser.getSession(function (err, session) {
-			if (err) {
-				alert(err.message || JSON.stringify(err));
-				return;
-			}
+    if (cognitoUser != null) {
+        cognitoUser.getSession(function (err, session) {
+            if (err) {
+                alert(err.message || JSON.stringify(err));
+                return;
+            }
 
-			if (session.isValid()) {
-				token = session.getIdToken().getJwtToken();
-			}
-		});
-	}
+            if (session.isValid()) {
+                token = session.getIdToken().getJwtToken();
+            }
+        });
+    }
 
-	return token
+    return token
 }
 
 const wsLink = new WebSocketLink({
-	uri: process.env.REACT_APP_GQL_REALTIME_ENDPOINT,
-	options: {
-		lazy: true,
-		reconnect: true,
-		connectionParams: async () => {
-			return {
-				headers: {
-					Authorization: getAuthToken(),
-				},
-				header: {
-					Authorization: getAuthToken(),
-				},
+    uri: process.env.REACT_APP_GQL_REALTIME_ENDPOINT,
+    options: {
+        lazy: true,
+        reconnect: true,
+        connectionParams: async () => {
+            return {
+                headers: {
+                    Authorization: getAuthToken(),
+                },
+                header: {
+                    Authorization: getAuthToken(),
+                },
 
-			}
-		},
-		// connectionCallback: (error) => {
-		// 	//@ts-ignore
-		// 	if (error?.message === "Authentication Failure!") {
-		// 		//@ts-ignore
-		// 		//wsLink.subscriptionClient.close(false, false);
-		// 	}
-		// },
-	},
+            }
+        },
+        // connectionCallback: (error) => {
+        // 	//@ts-ignore
+        // 	if (error?.message === "Authentication Failure!") {
+        // 		//@ts-ignore
+        // 		//wsLink.subscriptionClient.close(false, false);
+        // 	}
+        // },
+    },
 });
 
-console.log('wsLink',wsLink)
+console.log('wsLink', wsLink)
 
 const authLink = setContext((_, { headers }) => {
-	// get the authentication token from local storage if it exists
+    // get the authentication token from local storage if it exists
 
-	// return the headers to the context so httpLink can read them
-	return {
-		headers: {
-			...headers,
-			authorization: getAuthToken()
-		}
-	}
+    // return the headers to the context so httpLink can read them
+    return {
+        headers: {
+            ...headers,
+            authorization: getAuthToken()
+        }
+    }
 });
 
 const defaultOptions = {
-	watchQuery: {
-		fetchPolicy: 'no-cache',
-		errorPolicy: 'ignore',
-	},
-	query: {
-		fetchPolicy: 'no-cache',
-		errorPolicy: 'all',
-	},
+    watchQuery: {
+        fetchPolicy: 'no-cache',
+        errorPolicy: 'ignore',
+    },
+    query: {
+        fetchPolicy: 'no-cache',
+        errorPolicy: 'all',
+    },
 }
 
 const wsInterceptor = new ApolloLink((operation, forward) => {
 
-	console.log('wsInterceptor', {operation, forward})
-	return forward(operation)
+    console.log('wsInterceptor', { operation, forward })
+    return forward(operation)
 });
 
 const wsBranch = from[
-	// wsInterceptor,
-	wsLink
+    // wsInterceptor,
+    wsLink
 ]
 
 
 // header: Contains information relevant to the AWS AppSync endpoint and authorization. This is a base64 string encoded from a stringified JSON object. The JSON object content varies depending on the authorization mode.
 // payload: Base64 encoded string of payload.
 const splitWsHttpLink = split(
-	({ query }) => {
+    ({ query }) => {
 
-		const { kind, operation } = getMainDefinition(query);
-		console.log('link split ',{kind, operation, query})
-		return kind === 'OperationDefinition' && operation === 'subscription';
-	},
-	wsBranch,
-	httpLink,
+        const { kind, operation } = getMainDefinition(query);
+        console.log('link split ', { kind, operation, query })
+        return kind === 'OperationDefinition' && operation === 'subscription';
+    },
+    wsBranch,
+    httpLink,
 );
 
 
 
 const apolloClient = new ApolloClient({
-	// Provide required constructor fields
-	cache: cache,
-	link: from([authLink, errorLink, splitWsHttpLink]), //authLink.concat(link),
-	// uri: process.env.REACT_APP_GQL_API_ENDPOINT,
+    // Provide required constructor fields
+    cache: cache,
+    link: from([authLink, errorLink, splitWsHttpLink]), //authLink.concat(link),
+    // uri: process.env.REACT_APP_GQL_API_ENDPOINT,
 
-	// Provide some optional constructor fields
-	name: 'react-web-client',
-	version: '1.3',
-	queryDeduplication: false,
-	defaultOptions
+    // Provide some optional constructor fields
+    name: 'react-web-client',
+    version: '1.3',
+    queryDeduplication: false,
+    defaultOptions
 });
 
 export default apolloClient
