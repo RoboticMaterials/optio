3ade341f8c2e5a2288e577bb3928ecd30e9785ca
Davis <ddr95070@gmail.com>
2021-11-17 12:02:59 -0700

    newline wrapping

--- src/components/side_bar/content/cards/lot/lot_simple_row/lot_simple_row.style.js
+++ src/components/side_bar/content/cards/lot/lot_simple_row/lot_simple_row.style.js
@@ -8,7 +8,7 @@ export const Row = styled.div`
     display: flex;
     width: 100%;
     justify-content: space-between;
-    height: 1.8rem;
+    min-height: 1.8rem;
 
   	${props => props.isLast && lastItemCss};
 `
@@ -24,9 +24,10 @@ export const Label = styled.span`
 
 export const Count = styled.span`
 	font-size: ${props => props.theme.fontSize.sz4};
-    white-space: nowrap;
+    // white-space: nowrap;
     overflow: hidden;
     text-overflow: ellipsis;
     color:  ${props => props.theme.bg.septenary};
     font-family: ${props => props.theme.font.primary};
+    text-align: right;
 `
