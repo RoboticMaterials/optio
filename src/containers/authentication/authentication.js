c256c230bcff06cc4558693ca166d09194606b89
correll <nikolaus.correll@colorado.edu>
2022-02-25 21:31:24 -0700

    superfluous code removed

--- src/containers/authentication/authentication.js
+++ src/containers/authentication/authentication.js
@@ -95,17 +95,6 @@ const Authentication = (props) => {
                     }
                 });
 
-                cognitoUser.getUserAttributes(function(err, result) {
-                if (err) {
-                    alert(err.message || JSON.stringify(err));
-                    return;
-                }
-                for (i = 0; i < result.length; i++) {
-                    console.log(
-                        'attribute ' + result[i].getName() + ' has value ' + result[i].getValue()
-                    );
-                 }
-                });
             }
 
 
