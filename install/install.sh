0e789ecd06702eea91e67850342e2fe8d7819c69
root <root@ip-172-31-4-95.us-east-2.compute.internal>
2022-03-01 00:17:03 +0000

    libraries for socket

--- install/install.sh
+++ install/install.sh
@@ -51,6 +51,9 @@ pip3 install matplotlib
 pip3 install pymongo
 pip3 install cognitojwt
 pip3 install connexion[swagger-ui]
+pip3 install --upgrade python-socketio==4.6.0
+pip3 install --upgrade python-engineio==3.13.2
+pip3 install --upgrade Flask-SocketIO==4.3.1
 
 # Build Node
 echo "Building Node"
