5ae54d8c82ac157b31c4741d59a8a5aafaddfb65
opmtwo <opmtwo@outlook.com>
2022-05-02 10:30:49 +0530

    Allow all headers

--- serverless/app.py
+++ serverless/app.py
@@ -36,8 +36,7 @@ def before_request():
 @app.after_request
 def after_request(response):
     response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Headers',
-                         'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Headers', '*')
     response.headers.add('Access-Control-Allow-Methods',
                          'GET,PUT,POST,DELETE,OPTIONS')
     return response
