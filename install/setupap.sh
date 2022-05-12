a3c396eda0892f3ead4326482eb0f33cbb3670d7
Nikolaus Correll <nikolaus@optio.cloud>
2022-05-12 11:45:41 -0600

    setupap instructions added

--- install/setupap.sh
+++ install/setupap.sh
@@ -1,3 +1,4 @@
+# https://computingforgeeks.com/create-wi-fi-hotspot-on-ubuntu-debian-fedora-centos-arch/
 nmcli con add type wifi ifname wlp1s0 con-name optio-server autoconnect yes ssid optio-server
 nmcli con modify optio-server 802-11-wireless.mode ap 802-11-wireless.band bg ipv4.method shared
 nmcli con up optio-server
