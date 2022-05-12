4e72e054c0f71f6704ad092a0f20436793924fcf
Nikolaus Correll <nikolaus@optio.cloud>
2022-05-12 11:06:20 -0600

    setup ap script added

---
+++ install/setupap.sh
@@ -0,0 +1,5 @@
+nmcli con add type wifi ifname wlp1s0 con-name optio-server autoconnect yes ssid optio-server
+nmcli con modify optio-server 802-11-wireless.mode ap 802-11-wireless.band bg ipv4.method shared
+nmcli con up optio-server
+
+
