084e85ba7f89226707f52949a8db0fd67ad819a6
Nikolaus Correll <ncorrell@colorado.edu>
2026-06-03 03:00:43 -0600

    chore: Phase 1 dependency cleanup
    Frontend (package.json):
    - Remove 18 unused packages: aws-config, aws-s3, i18n, i18n-react, immer,
      jquery, lodash.debounce, pdf-viewer-reactjs, pdfjs (old), react-file-viewer,
      react-image-viewer-zoom, react-simple-image-viewer, reselect, store,
      url-parse, xmlrpc, redux-devtools, react-pdf, react-pdf-viewer (old)
    - Pin pdfjs-dist to exact version 2.6.347 (avoid 2.16.x private class fields
      breaking CRA 4 babel)
    - Remove import 'bootstrap' JS bundle from index.js (jquery peer dep not
      needed; CSS-only import retained for layout)
    - Keep @material-ui/core (required peer dep of @material-ui/icons)
    - Keep mongodb (used by scripts/postinstall.js to bump version serial)
    - Build verified: react-scripts build succeeds cleanly
    
    Python (requirements-legacy.txt):
    - Pin flask-socketio==5.3.6 (was unversioned)
    - Pin eventlet==0.33.3 (was unversioned)
    - Pin Pillow==12.2.0 (was missing entirely, PIL imported in code)
    
    Tests: 26/26 passing

--- src/index.js
+++ src/index.js
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './methods/css/montserrat.css';
 import './methods/css/iwawa.css';
 import './methods/css/all.css';
-import 'xmlrpc'
 import { Provider } from 'react-redux'
 import store from './redux/store/index.js'
 import './methods/css/fontawesome.min.css'
